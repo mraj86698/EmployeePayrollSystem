29aedfd334f1e2a9145514e70afd44fb989c9047
MohanRaj <mraj86698@gmail.com>
2022-08-31 18:57:00 +0530

    Added to Welcome Message For Employee Payroll

---
+++ EmployeePayroll/src/main/java/com/java/maven/EmployeePayroll/EmployeePayroll.java
@@ -0,0 +1,9 @@
+package com.java.maven.EmployeePayroll;
+
+public class EmployeePayroll
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee PayRoll Management System!" );
+    }
+}
---
+++ EmployeePayroll/src/test/java/com/java/maven/EmployeePayroll/EmployeePayrollTest.java
@@ -0,0 +1,16 @@
+package com.java.maven.EmployeePayroll;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class EmployeePayrollTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
