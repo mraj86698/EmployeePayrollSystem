554b177759f8b4b93cd35fb7d93b5c4e9282c44a
MohanRaj <mraj86698@gmail.com>
2022-08-31 19:26:42 +0530

    Added to Create Employee Pay Roll To Read And Write Console.

---
+++ EmployeePayroll/src/main/java/com/java/maven/EmployeePayroll/EmployeePayrollData.java
@@ -0,0 +1,36 @@
+package com.java.maven.EmployeePayroll;
+
+public class EmployeePayrollData {
+	public int id;
+	public String name;
+	public double salary;
+	/**
+	 * Parameterised Constructor
+	 * @param id
+	 * @param name
+	 * @param salary
+	 */
+	public EmployeePayrollData(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	/**
+	 * To Display the Employee Details
+	 */
+	public void printData() {
+		System.out.println("------------------------------------------------------");
+		System.out.println("Employee ID: "+id);
+		System.out.println("Employee Name: "+name);
+		System.out.println("Employee Salary: "+salary);
+		System.out.println("------------------------------------------------------");
+		System.out.println();
+	}
+	/**
+	 * It Returns the value into String Format
+	 */
+	@Override
+	public String toString() {
+		return "Created new Employee Data !";
+	}
+}
